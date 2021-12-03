4fd0788c211e57720ac8ef624fb3e406f002b9e1
Reinier Kip <dev@rjkip.nl>
2021-12-04 10:44:02 +1300

    Remove stray debug log

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -23,7 +23,6 @@
       if (latLng.lng < -180) latLng.lng = Math.ceil(-latLng.lng / 360) * 360 + latLng.lng;
       latLng.lat = ((latLng.lat + 90) % 180) - 90;
       latLng.lng = ((latLng.lng + 180) % 360) - 180;
-      console.log({ latLng });
     });
 
     return () => {
