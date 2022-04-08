cc3953de0721f73a430999ec679edcf8ff777dd7
Reinier Kip <dev@rjkip.nl>
2022-04-08 14:18:59 +1200

    Don't Minify for better Sentry traces?

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ const config = {
     adapter: adapter(),
 
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
+    target: '#svelte',
+
+    vite: {
+      build: { minify: false }
+    }
   }
 };
 
